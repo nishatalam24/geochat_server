79e45f3db0f35e5589824368c6711d59226b23b9
nishatalam24 <tajnoor2443@gmail.com>
2025-11-30 20:00:54 +0530

    latest

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const admin = require('firebase-admin');
 
-// Initialize Firebase Admin SDK (do this once at app startup)
-// const serviceAccount = require('./path/to/serviceAccountKey.json');
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
+// Initialize Firebase Admin SDK
+const serviceAccount = require('./geochat-841c4-firebase-adminsdk-fbsvc-a87cf7a06a.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 /**
  * Send FCM notification to Android device
@@ -14,7 +15,6 @@ const admin = require('firebase-admin');
  * @param {object} data - Optional data payload
  * @returns {Promise} - Firebase messaging response
  */
-// cElFOXiYTlmCDTJVzPbr4X:APA91bHqWDd6hK5Q6E7gc4uMxRNpCZ-esDD36irQ3T4PjCcrcGlMkQbx5YjiBG73_V6EjwgeLWhAPACqiUdBXcbIjeH3ql3QM13qoBH-PYGtTbwHm0OSzcw
 async function sendFCMNotification(deviceToken, title, body, data = {}) {
   const message = {
     token: deviceToken,
@@ -42,11 +42,8 @@ async function sendFCMNotification(deviceToken, title, body, data = {}) {
   }
 }
 
-// app.post('/api/test', (req, res) => {
-//   const payload = req.body;
-
-//   console.log('📦 Test API hit!', payload);
-//   console.log('📦 Webhook hit from Shiprocket');
-
+// let deviceToken = "dwJZinH5QtSbXMqwavnY6t:APA91bH4Zp-xP8s7iXCODIZGtKKrzAOQv5MEecSEcHoAza-r0A9qs44P4PPSXkVN-ZRbh-3RREi6uGEhudnN_P6nWZt1et9sK0oQNh7WUuLh45ehSx_JUo0"
+let deviceToken = "cWcSFUalS12cxUG4sCG4e8:APA91bHdVfJyR4nLx1CiczKC1dYIAHQTkFi2JNMTp4jQ-VFYqlJ2dK-JnUFukRzkCle8Xu5uhv7T7A0loG7EUQX5FwXfutlTY2qRAydRF0yTh89JPG9GJPY"
+sendFCMNotification(deviceToken, "Test Title", "This is a test notification body", { key1: 'value1', key2: 'value2' });
 
 module.exports = { sendFCMNotification };
